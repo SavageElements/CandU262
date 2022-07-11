28fa4b8f6196dd9cf1628903f9c5e73a2fa12e6c
J Rockwell Seebach <jrs@ironpinephoto.com>
2022-07-11 16:22:40 -0700

    Add project files.

---
+++ CandU262/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace CandU262
+{
+    class Program
+    {
+        public enum DaysOfWeek
+        {
+            monday = 1,
+            tuesday = 2,
+            wensday = 3,
+            thursday = 4,
+            friday = 5,
+            saturday = 6,
+            sunday = 7
+        }
+
+        public string Day { get; set; }
+
+        public int DayInt { get; set; }
+
+        static void Main(string[] args)
+        {
+            bool isToday = false;
+
+            while (isToday == false)
+            { 
+            Console.WriteLine("Please enter the current day of the week.");
+            string today = Console.ReadLine();
+            string lToday = today.ToLower();
+                try
+                {
+                    DaysOfWeek day = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), lToday);
+
+                    if ((int)day < (int)(DaysOfWeek.monday) | (int)day > (int)(DaysOfWeek.sunday))
+                    {
+                        Console.WriteLine("Please type in a day of the week.");
+                    }
+                    else if ((int)day >= (int)(DaysOfWeek.monday) | (int)day <= (int)(DaysOfWeek.sunday))
+                    {
+                        Console.WriteLine("Today is " + today);
+                        isToday = true;
+                    }
+                }
+                catch (ArgumentException)
+                {
+                    Console.WriteLine(today + " is not a day of the week.");
+                }
+            }
+        }
+    }
+}
